d71fa0147698f9549c279ab6bf07a5b13b84b94e
Thomas Gzuzstein <e.klein@students.htl-leonding.ac.at>
2023-11-10 21:41:16 +0100

    adasd

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-fetch(`http://localhost:8080/api/hello`, {}).then(response => {
-    response.json().then((data) => {
-        document.getElementById("pls").innerHTML = data;
-    });
-}).catch(err => {
+function sayHello() {
+    fetch(`http://localhost:8080/api/getEmployees`, {}).then(response => {
+        response.json().then((data) => {
+            document.getElementById("pls").innerHTML = data;
+        });
+    }).catch(err => {
 
-});
+    });
+}
