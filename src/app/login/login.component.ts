f342adf7bd0f9c460a435b6c64a04e003d58cff7
Thomas Gzuzstein <e.klein@students.htl-leonding.ac.at>
2023-12-17 22:08:18 +0100

    added a bunch of stuff

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  public registerMenu : boolean = false;
+  
+  public login(username : string, password : string) : boolean{
+    if(this.isAccountValid(username, password)){
+      localStorage.setItem('LoggedIn', 'true');
+      localStorage.setItem('Username', username);
+      localStorage.setItem('Password', password);
+    }
+    return true;
+  }
+  public register(username : string, password : string, email : string) : boolean{
+    fetch(`http://breneisminecraft.duckdns.org:5082/api/insertUser/${username}/${password}/${email}`, {
+  }).then(response => {
+    this.login(username, password);
+    return response.json();
+  }).catch(err => {
+    console.log(err);
+    return false;
+  });
+  return true;
+  }
+  public isAccountValid(username : string, password : string) : boolean{
+    fetch(`http://breneisminecraft.duckdns.org:5082/api/validUser/${username}/${password}`, {
+  }).then(response => {
+    return response.json();
+  }).catch(err => {
+    console.log(err);
+    return false;
+  });
+  return true;
+  }
 }
