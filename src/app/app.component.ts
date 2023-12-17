30557bab8c810b97a41f40ade2e080cc7981c77e
Thomas Gzuzstein <e.klein@students.htl-leonding.ac.at>
2023-12-17 20:24:56 +0100

    changed a few things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,61 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title : string = 'relace';
-  username : string = '';
-  password : string = '';
-  email : string = '';
-  users : string[] = [];
+  public title:string = 'relace';
 
-  constructor() {
-  }
-  public refresh(){
-    fetch(`http://breneisminecraft.duckdns.org:5082/api/getUsers`, {
-  }).then(response => {
-    response.json().then((data) => {
-      this.users = data;
-    });
-  }).catch(err => {
-
-  });
-  }
-  public login(){
-    console.log("login");
-    fetch(`http://breneisminecraft.duckdns.org:5082/api/validUser/${this.username}/${this.password}`, {
-  }).then(response => {
-    console.log(response.json());
-  }).catch(err => {
-
-  });
-  }
-  public register(){
-    fetch(`http://breneisminecraft.duckdns.org:5082/api/insertUser/${this.username}/${this.password}/${this.email}`, {
-  }).then(response => {
-    console.log(response);
-  }).catch(err => {
-
-  });
-    console.log("register");
-  }
-
-  public remove(){
-    console.log("login");
-    fetch(`http://breneisminecraft.duckdns.org:5082/api/removeAccount/${this.username}/${this.password}`, {
-  }).then(response => {
-    console.log(response.json());
-  }).catch(err => {
-
-  });
-  }
-  public onChange(htmlElement: any){
-    if(htmlElement.id == 'username'){
-      this.username = htmlElement.value;
-    }
-    if(htmlElement.id == 'password'){
-      this.password = htmlElement.value;
-    }
-    if(htmlElement.id == 'email'){
-      this.email = htmlElement.value;
-    }
+  isLoggedIn() : boolean{
+    return localStorage.getItem('LoggedIn') === 'true';
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    LoginComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
