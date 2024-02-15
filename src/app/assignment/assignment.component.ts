635aebafd4cb6b1087a77d8d5acc10e162dc0d4c
Leo-Breneis <114604133+Leo-Breneis@users.noreply.github.com>
2024-02-15 20:43:11 +0100

    update assignment

--- src/app/assignment/assignment.component.ts
+++ src/app/assignment/assignment.component.ts
@@ -32,6 +32,48 @@ export class AssignmentComponent {
     });
     return assignments;
   }
+
+  async getAllAssignmentsHTTP() {
+    let assignmentList = document.getElementById("assignmentList");
+    let username = document.getElementById("username") as HTMLInputElement;
+    let password = document.getElementById("password") as HTMLInputElement;
+    let token = this.fetchToken(username.value, password.value);
+    let assignments = await this.fetchAssignmentHTTP(token);
+    console.log(assignments);
+    assignmentList!.innerHTML = assignments.map((assignment: Assignment) => `
+    <div class="assignment">
+      <div class="assignment-title">${assignment.title.length <= 20 ? assignment.title : assignment.title.slice(0,18).concat("...")}</div>
+      <div class="assignment-description">${assignment.description.length < 25 ? assignment.description : assignment.description.slice(0,22) .concat("...") }</div>
+      <div class="assignment-duedate">${new Date(Number(assignment.dueDate)*1000).getDate()}/${new Date(Number(assignment.dueDate)*1000).getMonth()+1}/${new Date(Number(assignment.dueDate)*1000).getFullYear()-1984} n.Z.</div>
+      <div class="line"></div>
+      <div class="assignment-course">${assignment.course.length <= 15 ? assignment.course : assignment.course.slice(0,12).concat("...")}</div>
+      <div class="line"></div>
+      <div class="assignment-done">${assignment.completed?'✅':'❌'}</div>
+    </div>`).join("");
+    //div!.innerHTML = assignments.map((assignment: any) => `<tr><th>${assignment.title}</th><th>${assignment.description}</th><th><input type="checkbox" id="checkbox" ${assignment.completed ? "checked":""} ></th></tr>`).join(""); //<th><button id="deletebutton" onclick="deleteTodo(${todo.id})">DELETE</button></th>
+  }
+  async fetchAssignmentHTTP(token:Promise<String>) : Promise<Assignment[]> {
+    let tokenstr : String = await token;
+    let response = await fetch(`http://relacexyz.duckdns.org:5420/api/getAssignments/${tokenstr}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    
+    const assignments : Assignment[] = [];
+    const json = await response.json();
+    json.forEach((assignment: any) => {
+      assignments.push({
+        title: assignment.title,
+        description: assignment.description,
+        dueDate: assignment.dueDate,
+        completed: assignment.completed,
+        course: assignment.course
+      });
+    });
+    return assignments;
+  }
   
   async fetchToken(user:String, pass:String):Promise<String> {
     let response = await fetch("https://edufs.edu.htl-leonding.ac.at/moodle/login/token.php?username=" + user + "&password=" + pass + "&service=moodle_mobile_app")
