e908c61d7fb12aa4627b79502cbd82048f81e2d0
Eros-Klein <e.klein@students.htl-leonding.ac.at>
2024-03-01 19:59:44 +0100

    quick save

---
+++ src/home/home.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [HomeComponent]
+})
+export class HomeModule { }
--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,10 @@ import { AppModule } from './app/app.module';
 
 import { NavModule } from './nav/nav.module';
 
-import { HeadlineModule } from './headline/headline.module';
+import { HomeModule } from './home/home.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
 platformBrowserDynamic().bootstrapModule(NavModule)
-  .catch(err => console.error(err));
-
-platformBrowserDynamic().bootstrapModule(HeadlineModule)
   .catch(err => console.error(err));
